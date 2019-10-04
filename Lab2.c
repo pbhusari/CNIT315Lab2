26ad289913427a2d7b16aaf75bfac7b1cf850899
pranav <pranavbhusari2013@gmail.com>
2019-10-03 22:34:57 -0400

    Added ascii to binary conversion

--- Lab2.c
+++ Lab2.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+char *asciiToBin(char a);
+int factorial(int n);
+int fibbonacci(int n);
+void runHanoi(int n, char x, char y, char z);
+void lab2Reverse(char *userString, int start, int end);
+void lab2Ascii(char *userString, int start, int end);
+void binaryConversion(char target, int i);
+
+char binary[8];
 
 int main() {
-  int factorial(int n);
-  int fibbonacci(int n);
-  void runHanoi(int n, char x, char y, char z);
-  void lab2Reverse(char *userString, int start, int end);
 
-
+  int functionsExecuted = 0;
   int again = 1;
   int userChoice = 0;
   int userN = 0;
-  int i;
   char userString[256];
+
+
 
   while (again ==1){
     printf("Welcome to Lab 2, would you like to: \n");
@@ -31,19 +39,23 @@ int main() {
       case 1:
         printf("Please enter your number to find the factorial of: ");
         scanf("%i", &userN);
-        printf("Result: %i\n", factorial(userN));
+        printf("The factorial of %i is %i\n", userN, factorial(userN));
         userN = 0;
+        functionsExecuted++;
         break;
       case 2:
         printf("How many iterations of fibonacci: ");
         scanf("%i", &userN);
         printf("Result: ");
 
+        int i;
+
         for(i=0; i<userN; i++){
-          printf(" %i", fibbonacci(i));
+          printf("%i", fibbonacci(i));
         }
         printf("\n");
         userN = 0;
+        functionsExecuted++;
         break;
       case 3:
         printf("You have chosen towers of hanoi\n");
@@ -51,18 +63,25 @@ int main() {
         scanf("%i", &userN);
         runHanoi(userN, 'S', 'A', 'D');
         userN =0;
+        functionsExecuted++;
         break;
       case 4:
         printf("Enter the string you want to reverse: ");
-        scanf("%s", userString);
+        scanf(" %[^\n]", userString);
+        printf("%s reveresed is...\n", userString);
         lab2Reverse(userString, 0, strlen(userString) -1);
         printf("Reversed String: %s\n", userString);
+        functionsExecuted++;
         break;
       case 5:
         printf("You have chosen string to ascii\n");
+        scanf(" %[^\n]", userString);
+        lab2Ascii(userString, 0, strlen(userString) - 1);
+        functionsExecuted++;
         break;
       case 6:
         printf("You have chosen ascii to string\n");
+        functionsExecuted++;
         break;
       default:
         printf("Invalid Input\n");
@@ -72,6 +91,8 @@ int main() {
 		scanf("%i", &again);
 
   }
+
+  return functionsExecuted;
 
 }
 
@@ -104,6 +125,8 @@ void runHanoi(int n, char x, char y, char z){
 		printf("%c --> %c\n", x, z);
 		runHanoi(n-1,y,x,z);
 	}
+
+
 }
 
 void lab2Reverse(char *userString, int start, int end) {
@@ -122,5 +145,36 @@ void lab2Reverse(char *userString, int start, int end) {
     end--;
 
     lab2Reverse(userString, start, end);
+  }
+}
+
+void lab2Ascii(char *userString, int start, int end) {
+  int sum = 0;
+
+  if (end < start) {
+    printf("\n");
+    return;
+  }
+
+
+  binaryConversion(userString[start], 7);
+  printf("%s", binary);
+
+  lab2Ascii(userString, ++start, end);
+}
+
+void binaryConversion(char target, int i){
+  if (target == 0 && i==0) {
+    binary[i] = '0';
+    return;
+  }
+  else {
+    if (target%2 == 1) {
+      binary[i] = '1';
+    } else {
+      binary[i] = '0';
+    }
+
+    binaryConversion(target/2, i-1);
   }
 }
